f72f30281b267ef84acd2b2a6336a3f64fdc13f9
FinnChoi <hchoi524287@gmail.com>
2022-01-13 12:45:17 +0900

    22.01.13

--- script/main.js
+++ script/main.js
@@ -2,40 +2,54 @@
 onload = function(){
     const main = document.querySelector('.main-content__container');
     const week = document.querySelector('.week__body');
-        
+
     while(main.childElementCount != 7){
         main.append(week.cloneNode(true));
-    }    
+    } 
 
-    drawDate();
+    mainContent.drawMonth();
 }
-
-function drawDate(date){
-    if(!date || date.getDate() != 1){
-        const d = new Date();
-		date = new Date(d.getFullYear(), d.getMonth(), 1);
-    }
-
-    const title = document.querySelector('.header__text  > :first-child');
-    title.innerHTML = `${date.getFullYear()}년 ${date.getMonth()+1}월`;
-
-	const year = date.getFullYear();
-	const month = date.getMonth();
-	let day = date.getDay() == 0 ? -6 : -(date.getDay() - 1);
-
-	document.querySelectorAll('.week__body > .date').forEach((e)=>{
-		const d = new Date(year, month, day++);
+const headerContent = {
+    goToday : function(){
         
-        if(d.getMonth() != month){
-            let color = 'lightgrey';
+    }
+}
+const navContent = {
+    drawCalendar : function(){
 
-            if (d.getDay() == 0 ) color =  '#ffcdd2';
-            else if (d.getDay() == 6)  color = '#bbdefb';
-
-            e.setAttribute('style', 'color: ' + color);
+    }
+}
+const mainContent = {
+    drawMonth : function(date){
+        if(!date || date.getDate() != 1){
+            const d = new Date();
+            date = new Date(d.getFullYear(), d.getMonth(), 1);
         }
+    
+        const title = document.querySelector('.header__text  > :first-child');
+        title.innerHTML = `${date.getFullYear()}년 ${date.getMonth()+1}월`;
+    
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        let day = date.getDay() == 0 ? -6 : -(date.getDay() - 1);
+    
+        document.querySelectorAll('.week__body > .date').forEach((e)=>{
+            const d = new Date(year, month, day++);
+            
+            if(d.getMonth() != month){
+                let color = 'lightgrey';
+    
+                if (d.getDay() == 0 ) color =  '#ffcdd2';
+                else if (d.getDay() == 6)  color = '#bbdefb';
+    
+                e.setAttribute('style', 'color: ' + color);
+            }
+    
+            e.innerHTML = d.getDate();
+            e.setAttribute('date', `${new Date( + d + 3240 * 10000).toISOString().split("T")[0]}`);
+        });
+    },
+    drawWeek : function(date){
 
-        e.innerHTML = d.getDate();
-        e.setAttribute('date', `${new Date( + d + 3240 * 10000).toISOString().split("T")[0]}`);
-	});
+    }
 }
